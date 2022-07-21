7c28d3d1f6a82de328315b00c9c6d8888b23928e
stephen rogan <srogan26@gmail.com>
2022-07-20 22:41:54 -0500

    started working with a json data file and introduced a post request

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
 const path = require('path');
+const fs = require('fs');
+const notesFile = '../../notes.json'
 
 router.get('/', (req, res) => {
-    res.status(200).json({})
-})
+    const noteData = fs.readFileSync(path.join(__dirname,notesFile), {encoding: 'utf-8'});
+    const parsedNoteData = JSON.parse(noteData);
+    res.status(200).json(parsedNoteData);
+});
+
+router.post("/", (req,res)=>{
+    console.log(req.body);
+    //write function that takes the request body
+    //and adds that note to the list of notes
+    res.status(200).send('request received');
+});
 
 module.exports = router;
