e976016bcfefe423b51e0f0f49c93287f2fe46b0
stephen rogan <srogan26@gmail.com>
2022-07-25 20:39:24 -0500

    created post request functionality to add new notes to notes json file

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -3,16 +3,24 @@ const path = require('path');
 const fs = require('fs');
 const notesFile = '../../notes.json'
 
-router.get('/', (req, res) => {
+const readParsedNotesFile = () => {
     const noteData = fs.readFileSync(path.join(__dirname,notesFile), {encoding: 'utf-8'});
     const parsedNoteData = JSON.parse(noteData);
+    return parsedNoteData;
+}
+
+router.get('/', (req, res) => {
+    const parsedNoteData = readParsedNotesFile();
     res.status(200).json(parsedNoteData);
 });
 
 router.post("/", (req,res)=>{
-    console.log(req.body);
+    const parsedNoteData = readParsedNotesFile();
+    parsedNoteData.push(req.body);
     //write function that takes the request body
+    const stringifiedNotes = JSON.stringify(parsedNoteData);
     //and adds that note to the list of notes
+    fs.writeFileSync(path.join(__dirname,notesFile), stringifiedNotes);
     res.status(200).send('request received');
 });
 
